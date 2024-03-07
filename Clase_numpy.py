2931de0a172371d0a6abaff9b896c08095e25729
Randy9560 <r.garcia@udea.edu.co>
2024-03-07 13:39:18 -0500

    Nueva clase

---
+++ Clase_numpy.py
@@ -0,0 +1,21 @@
+import numpy as np 
+
+b = np.arange(15)
+
+
+a = np.arange(20).reshape(10,-1) #El primer numero me indica la cantidad de filas que quiero, el seguno es de cuantos componentes quiero la fila, pongo -1 el solo me calcula el valor
+# print(a)
+
+c = np.array([(2,3,6),(6.5,4,6)])
+# print(c)
+
+# print(a.ndim) #El objeto.ndim me dice la dimension del objero
+
+# print(a.shape) #Me indica la tupla de como esta desde 2D fromado con (filas,columnas)
+
+# print(b.size) #Me dice el numero de elementos que tiene el array 
+
+# print(a.dtype) #Me inidica el tipo de datos que hay dentro del array
+
+d = np.array([[1,2],[3,4]], dtype=complex)
+print(d)
