725cbb38110172760658bf7afce8e1889d59a1d8
Randy9560 <r.garcia@udea.edu.co>
2024-05-31 01:02:38 -0500

    El modelo esta casi listo

---
+++ Unidad 4/Trabajo monitor/Modelo.py
@@ -0,0 +1,58 @@
+import json
+
+
+class Modelo(object):
+    def __init__(self, base_datos = 'datos.json'):
+        self.base_datos = base_datos
+        self.load()
+        self.__usuarios = {}
+        self.__usuarios['contrasena123'] = 'admin123'
+        self.__usuarios['admin'] = 'admin'
+
+    def load(self):
+        try:
+            with open(self.base_datos, 'r') as archivo:
+                self.datos = json.load(archivo)
+        except FileNotFoundError:
+            self.datos = []
+            print("No se ha encontrado el archivo con la base de datos")
+    
+    def verificarUsuario(self, u:str, p:str):
+        try:
+            if self.__usuarios[p] == u:
+                return True
+            else:
+                return False
+        except: 
+            return False
+        
+    def guardar_info(self):
+        with open(self.base_datos, 'w') as archivo:
+            json.dump(self.datos,archivo, indent = 4)
+
+    def creador_de_diccionarios(self,n:str,a:str,id:str, edad:str): #Primordial hacer este punto primero
+        self.dic =       {
+                        'Nombre': f'{n}',
+                        'Apellido': f'{a}',
+                        'Edad': f'{edad}',
+                        'Identificacion': f'{id}'                            
+                        
+                    }
+        return self.dic
+    
+    def añadir_paciente(self):
+        paciente = self.dic
+        if not any(p['Identificacion'] == paciente['Identificacion'] for p in self.datos):
+            self.datos.append(paciente)
+            self.guardar_info()
+            return True
+        return False
+    
+    def eliminar_paciente(self, id_paciente:str):
+        self.datos = [p for p in self.datos if p['Identificacion'] != id_paciente]
+
+    def buscar_paciente(self, nombre_paciente:str):
+        nombre_paciente = nombre_paciente.lower().strip()
+        return [p for p in self.datos if p['Identificacion'].startswith(nombre_paciente)]
+    
+        
---
+++ Unidad 4/Trabajo monitor/controlador.py
@@ -0,0 +1,1 @@
+from Modelo import *
